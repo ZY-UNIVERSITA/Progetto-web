40a1774eac81557293b215df634aed7c69e0f949
ZY <yuhang.zhu@studio.unibo.it>
2024-11-14 01:49:15 +0100

    Add db connection
    Added db connection to connect to remote database

---
+++ Social Network Backend/src/utils/db.ts
@@ -0,0 +1,12 @@
+import mysql, { Connection } from "mysql2";
+
+const connection: Connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "socialnetwork"
+});
+
+// Esporta la connessione per poter essere usato all'esterno
+// default permette di esportarlo e usarlo come import senza specificare il componente da importare da un modulo
+export default connection;
