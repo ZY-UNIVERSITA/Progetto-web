1c1b12e88228d67b4bf7ec5ee0ea3bba9126fcd8
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-17 15:55:12 +0100

    Delete import styles from main.ts

--- Social Network Frontend/src/main.ts
+++ Social Network Frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory, Router } from 'vue-router'
-import './style.css'
 import App from './App.vue'
 import Home from './pages/Home.vue';
 import NotFound from "./pages/NotFound.vue"
