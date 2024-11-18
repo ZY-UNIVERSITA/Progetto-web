3841f6a348a9dccdfab38ac279d18cd656cb611a
ZY <yuhang.zhu@studio.unibo.it>
2024-11-18 21:38:03 +0100

    Add new backend logic

--- Social Network Backend/src/controllers/posts-controller.ts
+++ Social Network Backend/src/controllers/posts-controller.ts
@@ -1,29 +1,63 @@
 import { Request, Response } from "express";
 import executeQuerySQL from "../utils/querySQL";
-import { User } from "../utils/types";
+import { postID_interface, User } from "../utils/types";
 import { getUser } from "../utils/auth";
+
 
 const publicVisibility: string = "public";
 const friendVisibility: string = "friends";
 
 
 // First class function con funzione anonima di ts che ritorna una promise di tipo void
+
+
+// LAVORARCI PENSANDO CHE SI DEBBA FARE TUTTE LE CHIAMATE AXIOS
+/*
 export const postID = async (req: Request, res: Response): Promise<void> => {
-    const postID: string = req.params.id;
+    const { user_id, username, visibility }: postID_interface = req.body;
+    const post_id = req.params.id;
+
+    const user: User | null = getUser(req, res);
+
+    if (visibility === "friends") {
+        if (user === null) {
+            res.status(400).send("Not found or not friend");
+            return;
+        } 
+
+        const confirmIfFriendQuerySQL: string = 
+        `
+            SELECT *
+            FROM follower as f
+            WHERE f.following_user_id = ? AND f.follower_user_id = ?
+        `
+        const result: any[] = await executeQuerySQL(req, res, confirmIfFriendQuerySQL, false, user_id, user.user_id);
+
+        if (result.length === 0) {
+            res.status(400).send("Not found or not friend");
+            return;
+        }
+        }
+    }
 
     const querySQL: string = 
     `
-        SELECT p.post_id, p.user_id, p.content, p.created_at, p.visibility, p.likes, p.comments, p.shares, u.username, u.full_name
+        SELECT p.post_id, p.user_id, p.content, p.created_at, p.likes, p.comments, p.shares, u.username, u.full_name, u.visibility
         FROM posts as p JOIN users as u ON (p.user_id = u.user_id)
         WHERE p.post_id = ? AND p.visibility LIKE ?
     `;
 
-    await executeQuerySQL(req, res, querySQL, true, postID, publicVisibility);
-};
+    
+
+    await executeQuerySQL(req, res, querySQL, true, post_id, publicVisibility);
+};*/
 
 
 export const postsUser = async (req: Request, res: Response): Promise<void> => {
     const userID: string = req.params.id;
+
+    const user: User | null = getUser(req, res);
+
 
     const querySQL: string = 
     `
@@ -42,11 +76,11 @@ export const popularPosts = async (req: Request, res: Response): Promise<void> =
     if (!user) {
         const querySQL: string = 
         `
-            SELECT p.post_id, p.user_id, p.content, p.created_at, p.visibility, p.likes, p.comments, p.shares, u.username, u.full_name
+            SELECT p.post_id, p.user_id, p.content, p.created_at, p.likes, p.comments, p.shares, u.username, u.full_name, u.visibility
             FROM posts as p JOIN users as u ON (p.user_id = u.user_id)
 
             -- Permette di selezionare solo i post non più vecchi di 30 giorni rispetto al momento in cui si effettua la query
-            WHERE p.visibility LIKE ? AND p.created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+            WHERE u.visibility LIKE ? AND p.created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY)
 
             -- Permette di ordinare i valori tramite un algoritmo di ranking basato su un punteggio ponderato
             -- Il numero di likes, commenti e shares permette di ottenere un punteggio elevato
@@ -58,11 +92,11 @@ export const popularPosts = async (req: Request, res: Response): Promise<void> =
     } else {
         const querySQL: string = 
         `
-            SELECT p.post_id, p.user_id, p.content, p.created_at, p.visibility, p.likes, p.comments, p.shares, u.username, u.full_name, f.following_user_id, f.follower_user_id
+            SELECT p.post_id, p.user_id, p.content, p.created_at, p.likes, p.comments, p.shares, u.username, u.full_name, u.visibility
             FROM posts as p JOIN users as u ON (p.user_id = u.user_id) LEFT JOIN follower as f ON p.user_id = f.following_user_id
 
             -- Permette di selezionare solo i post non più vecchi di 30 giorni rispetto al momento in cui si effettua la query
-            WHERE (p.visibility LIKE ? OR (p.visibility LIKE ? AND f.follower_user_id LIKE ?)) AND p.created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY)
+            WHERE (u.visibility LIKE ? OR (u.visibility LIKE ? AND f.follower_user_id LIKE ?)) AND p.created_at >= DATE_SUB(NOW(), INTERVAL 30 DAY)
 
             -- Permette di ordinare i valori tramite un algoritmo di ranking basato su un punteggio ponderato
             -- Il numero di likes, commenti e shares permette di ottenere un punteggio elevato
--- Social Network Backend/src/controllers/users-controller.ts
+++ Social Network Backend/src/controllers/users-controller.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from "express"
 import executeQuerySQL from "../utils/querySQL";
 
+
+const publicVisibility: string = "public";
+const friendVisibility: string = "friends";
+
+
 // First class function con funzione anonima di ts che ritorna una promise di tipo void
 export const userProfile = async (req: Request, res: Response): Promise<void> => {
     const username: string = req.params.username;
 
-    // Diventerà una stringa di valore dinamico
-    const visibility: string = "public";
-
     // Viene eseguito una query SQL
     const querySQL: string =
         `
-        SELECT u.username, u.full_name, u.bio, u.birth_date, u.profile_picture, u.banner_picture
+        SELECT u.user_id, u.username, u.full_name, u.visibility, u.bio, u.birth_date, u.profile_picture, u.banner_picture
         FROM users as u
-        WHERE u.username = ? AND u.visibility LIKE ?
+        WHERE u.username = ?
     `;
 
-    await executeQuerySQL(req, res, querySQL, true, username, visibility);
+    await executeQuerySQL(req, res, querySQL, true, username);
 }
