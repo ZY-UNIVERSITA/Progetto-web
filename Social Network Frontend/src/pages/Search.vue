50593888db68154213521ae18f2cad8242b5e395
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-21 23:06:49 +0100

    Add template and style to Search page

--- Social Network Frontend/src/pages/Search.vue
+++ Social Network Frontend/src/pages/Search.vue
@@ -1,3 +1,17 @@
 <template>
-    <p>prova</p>
-</template>
+    <div class="search-container">
+      <!-- Casella di ricerca -->
+      <input type="text" v-model="searchQuery" @input="handleSearch" placeholder="Search..."/>
+        <!--
+            <section v-if="searchResults.length">
+                <section v-for="result in searchResults" :key="result.id" class="search-result">
+                <p>{{ result.name || result.content }}</p>
+                </section>
+            </section>
+        -->
+        <!-- section v-else -->
+      <section>
+        <p>No results found.</p>
+      </section>
+    </div>
+  </template>
