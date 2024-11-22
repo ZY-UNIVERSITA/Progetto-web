3dc361b776cce38e5f883fcb16992707e15b62b4
ZY <yuhang.zhu@studio.unibo.it>
2024-11-22 10:14:35 +0100

    Update singlePostComponent.vue

--- Social Network Frontend/src/components/singlePostComponent.vue
+++ Social Network Frontend/src/components/singlePostComponent.vue
@@ -5,8 +5,9 @@
 
     export default defineComponent({
         data() {
-        return {
-        }
+            return {
+                addedLike: false as boolean,
+            }
         },
         props: {
             post: {
@@ -17,8 +18,7 @@
         methods: {
             async like(event: Event): Promise<void> {
                 const elem = event.currentTarget as SVGElement;
-                elem.classList.toggle("red");
-                elem.classList.toggle("pulse");
+                this.addedLike = !this.addedLike;
                 
                 try {
                     await axios.post("/api/post/like/", {
@@ -59,7 +59,7 @@
             <p>{{ post.created_at }}</p>
 
             <!-- click.stop permette di mettere like senza andare al post-->
-            <svg v-on:click.stop="like" id="heart" class="heart" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg v-on:click.stop="like" id="heart" class="heart" :class="{'red pulse': addedLike}" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
         </section>
