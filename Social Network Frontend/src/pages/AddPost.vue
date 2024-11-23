01b19f88bec1f729b3e1019932d32c8ca9b8fdd5
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-23 22:33:11 +0100

    Start style of page AddPost

--- Social Network Frontend/src/pages/AddPost.vue
+++ Social Network Frontend/src/pages/AddPost.vue
@@ -1,12 +1,12 @@
 <template>
     <form action="#" method="POST" enctype="multipart/form-data">
         <textarea name="postContent" placeholder="Write here..."></textarea>
-            
+        
         <label for="imageUpload" class="add-img-btn">
             Add img
             <input type="file" id="imageUpload" name="image" style="display:none;">
         </label>
-                
+            
         <button type="button" class="cancel-btn" onclick="clearForm()">Cancel</button>
         <button type="submit" class="publish-btn">Publish</button>
     </form>
