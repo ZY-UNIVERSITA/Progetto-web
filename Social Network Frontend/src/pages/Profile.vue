4b1806512b98c89f991350bfe8eea81cecc516fa
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-27 20:31:38 +0100

    Start template and script of page Profile

--- Social Network Frontend/src/pages/Profile.vue
+++ Social Network Frontend/src/pages/Profile.vue
@@ -1,3 +1,59 @@
+<script lang="ts">
+    import { defineComponent, PropType } from 'vue';
+    import axios from 'axios';
+    import { Post, UserToken     } from '../utils/types';
+    import singlePostComponent from '../components/singlePostComponent.vue';
+
+    export default defineComponent({
+        components: { 
+            singlePostComponent
+        },
+        props: {
+            user: {
+                type: Object as PropType<UserToken | null>,
+                required: false,
+            },
+        },
+        data() {
+            return {
+                posts: [] as Post[],
+            }
+        },
+        methods: {
+            async getPopularPosts() {
+                try {
+                    const results: any = await axios.get("/api/popularPosts");
+                    this.posts = results.data;
+                } catch (e: any) {
+                    console.error(e);
+                }
+            },
+            goToPost(postID: string) {
+                this.$router.push({ 
+                    name: 'SinglePost', 
+                    params: { "id": postID }
+                });
+            }
+        },
+        created() {
+            this.getPopularPosts(); 
+        }
+    });
+</script>
+
 <template>
-    <p>User personal profile</p>
+    <!--<img src="path_to_profile_image.jpg" alt="Immagine Profilo" class="profile-img">-->
+    <h2 class="username">Username</h2>
+    
+    <label class="switch">
+        <input type="checkbox" id="theme-toggle">
+        <span class="slider"></span>
+    </label>
+
+    <section id="popularPosts">
+        <template v-for="post in posts">
+            <singlePostComponent :post="post" :user="user" v-on:click="goToPost(post.post_id)"></singlePostComponent>
+        </template>
+    </section>
 </template>
+/
