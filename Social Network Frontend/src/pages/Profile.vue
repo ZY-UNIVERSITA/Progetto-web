7bb2d7d53c8a9d307320bcddccca8dd2c26ae031
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-12-27 21:00:45 +0100

    Update profile page style

--- Social Network Frontend/src/pages/Profile.vue
+++ Social Network Frontend/src/pages/Profile.vue
@@ -2,7 +2,7 @@
 import { defineComponent, PropType } from 'vue';
 import axios from 'axios';
 import { Post, User, UserToken } from '../utils/types';
-import SinglePostComponent from '../components/SinglePostComponent.vue';
+import SinglePostComponent from '../components/singlePostComponent.vue';
 import ProfileBanner from '../components/ProfileBanner.vue';
 
 export default defineComponent({
