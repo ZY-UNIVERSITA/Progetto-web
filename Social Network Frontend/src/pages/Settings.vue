89e160a57117aca55c839390baebdfe1813764ac
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-01-01 15:54:55 +0100

    Update api and info in Settings page

--- Social Network Frontend/src/pages/Settings.vue
+++ Social Network Frontend/src/pages/Settings.vue
@@ -1,89 +1,126 @@
 <script lang="ts">
-  import { defineComponent, reactive } from 'vue';
+import axios from 'axios';
+import { defineComponent } from 'vue';
+import { User } from '../utils/types';
   
   export default defineComponent({
     name: 'Settings',
-    setup() {
-      const userInfo = reactive({
-        username: '',
-        email: '',
-      });
-  
+    data() {
+        return {
+          user: {} as User,
 
-      /**
-       * api/user/:username per ottenere le info utente:
-       * bio
-       * user birth date
-       * full_name
-       * visibility
-       * 
-       * 
-       */
-
-       /**
-        * /api/account/information/",
-        * per inviare quei 4 dati sopra da aggiornare.
-        */
-
-        /**
-         * api/account/password/"
-         * per poter aggiornare la password
-        */
-       /**
-        * /api/account/delete/",
-        * per poter eliminare l'account
-         */
-        
-      const updateName = () => {
-        alert(`Informazioni aggiornate`);
-        // Logica per inviare i dati aggiornati al backend
-      };
-
-      const updateEmail = () => {
-        alert(`Informazioni aggiornate`);
-        // Logica per inviare i dati aggiornati al backend
-      };
-  
-      const changePassword = () => {
-        alert('Password cambiata!');
-        // Logica per cambiare la password
-      };
-  
-      const deleteAccount = () => {
-        if (window.confirm('Sei sicuro di voler eliminare il tuo account?')) {
-          alert('Account eliminato.');
-          // Logica per eliminare l'account dal backend
-        }
-      };
-  
-      return { userInfo, updateName, updateEmail, changePassword, deleteAccount };
+          oldPassword: '',
+          newPassword: ''
+        };
     },
-  });
+    methods: {
+      async loadUserInfo() {
+        try {
+          const response = await axios.get(`/api/user/${this.user.username}`);
+          this.user = response.data;
+        } catch (error) {
+          console.error('Errore nel caricare le informazioni utente:', error);
+        }
+      },
+      async updateInfo() {
+        try {
+          const updatedUser = {
+            full_name: this.user.full_name,
+            birth_date: this.user.birth_date,
+            bio: this.user.bio,
+            visibility: this.user.visibility
+          };
+
+          await axios.put('/api/account/information/', updatedUser);
+          alert('Informazioni aggiornate');
+        } catch (error) {
+          console.error('Errore nell\'aggiornare le informazioni:', error);
+          alert('Si è verificato un errore nell\'aggiornare le informazioni.');
+        }
+      },
+      async changePassword() {
+        const oldPasswordValue = this.oldPassword;
+        const newPasswordValue = this.newPassword;
+
+        try {
+          const response = await axios.put('/api/account/password/', {
+            oldPassword: oldPasswordValue,
+            newPassword: newPasswordValue
+          });
+
+          if (response.status === 200) {
+            alert('Password cambiata con successo!');
+          } else {
+            alert('Errore nel cambiare la password. Assicurati che la vecchia password sia corretta.');
+          }
+        } catch (error) {
+          console.error('Errore durante il cambiamento della password:', error);
+          alert('Si è verificato un errore durante il cambiamento della password. Riprova più tardi.');
+        }
+      },
+
+      async deleteAccount() {
+        if (window.confirm('Sei sicuro di voler eliminare il tuo account?')) {
+          try {
+            const response = await axios.delete('/api/account/delete/');
+
+            if (response.status === 200) {
+              alert('Il tuo account è stato eliminato con successo.');
+              window.location.href = '/';
+            } else {
+              alert('Si è verificato un errore durante l\'eliminazione dell\'account.');
+            }
+          } catch (error) {
+            console.error('Errore durante l\'eliminazione dell\'account:', error);
+            alert('Si è verificato un errore durante l\'eliminazione dell\'account.');
+          }
+        }
+      }
+  },
+  created() { this.loadUserInfo(); }
+});
 </script>
 
 <template>
-    <div class="settings-container">
-  
+  <div class="settings-container">
+
     <!-- Modifica informazioni -->
-    <section class="settings-section">
-        <h2>Modifica Informazioni</h2>
-        <label>
-          Nome utente:
-          <input v-model="userInfo.username" type="text" placeholder="Inserisci nuovo nome utente" />
-        </label>
-        <button class="update-button" @click="updateName">Aggiorna nome</button>
-        <label>
-          Email:
-          <input v-model="userInfo.email" type="email" placeholder="Inserisci nuova email" />
-        </label>
-        <button class="update-button" @click="updateEmail">Aggiorna email</button>
+    <section class="settings-section" v-if="user">
+      <h2>Modifica Informazioni</h2>
+      <label>
+        Full name:
+        <input v-model="user.full_name" type="text" placeholder="Inserisci nuovo nome" />
+      </label>
+      <label>
+        Birth date:
+        <input v-model="user.birth_date" type="date" placeholder="Inserisci nuova data di nascita" />
+      </label>
+      <label>
+        Bio:
+        <input v-model="user.bio" type="text" placeholder="Inserisci nuovo bio" />
+      </label>
+      <label>
+        Visibility:
+        <select v-model="user.visibility" name="visibility">
+          <option value="public">Public</option>
+          <option value="private">Private</option>
+        </select>
+      </label>
+      <button class="update-button" @click="updateInfo">Aggiorna info</button>
     </section>
-  
+
     <!-- Cambia password -->
     <section class="settings-section">
-        <h2>Cambia Password</h2>
-        <input type="password" placeholder="Inserisci nuova password" minlength="16" />
-        <button class="update-button" @click="changePassword">Cambia Password</button>
+      <h2>Cambia Password</h2>
+      <label>
+        Vecchia password:
+        <input v-model="oldPassword" type="password" minlength="16" placeholder="Inserisci vecchia password" />
+      </label>
+      <label>
+        Nuova password:
+        <input v-model="newPassword" type="password" minlength="16" placeholder="Inserisci nuova password" />
+      </label>
+      <button class="update-button" @click="changePassword">Cambia Password</button>
     </section>
   
     <!-- Elimina account -->
@@ -92,6 +129,6 @@
         <button class="delete-button" @click="deleteAccount">Elimina Account</button>
     </section>
     
-    </div>
+  </div>
 </template>
   
