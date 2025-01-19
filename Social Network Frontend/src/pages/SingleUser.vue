a0ca5d7583b799b0841ee81557b22e2abd965a1d
ZY <yuhang.zhu@studio.unibo.it>
2025-01-19 21:16:49 +0100

    Fix format

--- Social Network Frontend/src/pages/SingleUser.vue
+++ Social Network Frontend/src/pages/SingleUser.vue
@@ -1,70 +1,70 @@
 <script lang="ts">
-    import { defineComponent, PropType } from 'vue';
-    import axios from 'axios';
-    import { Post, User, UserToken } from '../utils/types';
-    import singlePostComponent from '../components/singlePostComponent.vue';
-    import NotFound from './NotFound.vue';
+import { defineComponent, PropType } from 'vue';
+import axios from 'axios';
+import { Post, User, UserToken } from '../utils/types';
+import singlePostComponent from '../components/singlePostComponent.vue';
+import NotFound from './NotFound.vue';
 import ProfileBanner from '../components/ProfileBanner.vue';
 
-    export default defineComponent({
-        components: { 
-            singlePostComponent,
-            NotFound,
-            ProfileBanner
+export default defineComponent({
+    components: {
+        singlePostComponent,
+        NotFound,
+        ProfileBanner
+    },
+    props: {
+        user: {
+            type: Object as PropType<UserToken | null>,
+            required: false,
         },
-        props: {
-            user: {
-                type: Object as PropType<UserToken | null>,
-                required: false,
-            },
-        },
-        data() {
-            return {
-                posts: [] as Post[],
-                userProfile: null as User | null,
-            }
-        },
-        methods: {
-            async fetchUserPosts() {
-                try {
-                    const profile = await axios.get("api/user/" + this.$route.params.username);
-                    this.userProfile = profile.data[0];        
-                    
-                    if (this.userProfile !== null) {
-                        console.log("io entro", this.userProfile);
-                        const results: any = await axios.get("/api/posts/user/" + this.userProfile.username);
-                        this.posts = results.data;
-                    }
-                } catch (e: any) {
-                    console.log(e);
+    },
+    data() {
+        return {
+            posts: [] as Post[],
+            userProfile: null as User | null,
+        }
+    },
+    methods: {
+        async fetchUserPosts() {
+            try {
+                const profile = await axios.get("api/user/" + this.$route.params.username);
+                this.userProfile = profile.data[0];
+
+                if (this.userProfile !== null) {
+                    console.log("io entro", this.userProfile);
+                    const results: any = await axios.get("/api/posts/user/" + this.userProfile.username);
+                    this.posts = results.data;
                 }
-            },
-            goToPost(postID: string) {
-                this.$router.push({ 
-                    name: 'SinglePost', 
-                    params: { "id": postID }
-                });
+            } catch (e: any) {
+                console.log(e);
             }
         },
-        watch: {
-            '$route.params': {
-                handler() {
-                    // resetta il post
-                    this.posts = [];
-                    this.userProfile = null;
-                    // Fai una nuova fetch
-                    this.fetchUserPosts();
-                    console.log("faccio chiamata api");
-                },
-                immediate: true,
+        goToPost(postID: string) {
+            this.$router.push({
+                name: 'SinglePost',
+                params: { "id": postID }
+            });
+        }
+    },
+    watch: {
+        '$route.params': {
+            handler() {
+                // resetta il post
+                this.posts = [];
+                this.userProfile = null;
+                // Fai una nuova fetch
+                this.fetchUserPosts();
+                console.log("faccio chiamata api");
             },
+            immediate: true,
         },
-    });
+    },
+});
 </script>
 
 <template>
     <section id="user_profile">
-        <template v-if="userProfile">   
+        <template v-if="userProfile">
             <ProfileBanner :user="user"></ProfileBanner>
 
             <section id="userPosts">
@@ -72,14 +72,28 @@ import ProfileBanner from '../components/ProfileBanner.vue';
                     <p>L'utente cercato è privato. Effettua il login oppure registrati per continuare.</p>
                 </template>
                 <template v-else v-for="post in posts">
-                    <singlePostComponent :post="post" :user="user" v-on:click="goToPost(post.post_id)"></singlePostComponent>
+                    <singlePostComponent :post="post" :user="user" v-on:click="goToPost(post.post_id)">
+                    </singlePostComponent>
                 </template>
-            </section> 
+            </section>
 
         </template>
 
         <template v-else>
-            <NotFound></NotFound>    
+            <NotFound></NotFound>
         </template>
     </section>
 </template>
+
+<style lang="scss" scoped>
+#userPosts {
+    article {
+        border-bottom: solid white;
+        border-radius: 0px;
+
+        &:last-child {
+            border-bottom: none;
+        }
+    }
+}
+</style>
